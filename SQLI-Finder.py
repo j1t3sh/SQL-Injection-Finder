5e17abb00332c850d3ca4a0e30fe299b23f445ac
j1t3sh <jitesh101.kumar@gmail.com>
2020-02-04 03:51:48 +0530

    Fix some issues

--- SQLI-Finder.py
+++ SQLI-Finder.py
@@ -4,17 +4,20 @@ import sys
 import urllib
 import urllib.request
 from termcolor import colored
+import random
 
 website_list=[]
 #query = "inurl:php?id= site:.com"
 dork = "inurl:" + input(colored("Please input the sqli Dork(eg- php?id=, aspx?id=) ---->  ",'green'))
 extension = "site:" + input(colored("Please specify the website extension(eg- .in,.com,.pk) -----> ",'green'))
 total_output = int(input(colored("Pleases specify the total no. of websites you want) ----> ",'green')))
+page_no = int(input(colored("From which Google page you want to start(eg- 1,2,3) ----> ",'green')))
 query = dork + " " +  extension 
-for j in search(query, num=10,stop=total_output, pause=5): 
+pause_random = int(random.randrange(3, 10, 1))
+for j in search(query, num=10,start=page_no*10,stop=total_output, pause=pause_random): 
     website_list.append(j) 
-
-for i in website_list:
+print(website_list)
+'''for i in website_list:
         try:
             fullurl = i
 
@@ -29,5 +32,4 @@ for i in website_list:
         except:
             print(i + "  ===> " + colored(" Cannot be Determined",'blue'))
             continue
-
-#hii
+'''
