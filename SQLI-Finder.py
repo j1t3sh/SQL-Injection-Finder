a6712eac5b9765eef97deff0399e68bd04e66cef
j1t3sh <jitesh101.kumar@gmail.com>
2020-02-06 04:41:48 +0530

    Issues Fixed

--- SQLI-Finder.py
+++ SQLI-Finder.py
@@ -37,28 +37,31 @@ banner_terminal = terminal_banner.Banner(banner)
 print (colored(banner_terminal, 'red')+ "\n")
 
 website_list=[]
-#query = "inurl:php?id= site:.com"
 dork = "inurl:" + input(colored("Please input the sqli Dork(eg- php?id=, aspx?id=) ---->  ",'cyan'))
 extension = "site:" + input(colored("Please specify the website extension(eg- .in,.com,.pk) -----> ",'cyan'))
 total_output = int(input(colored("Pleases specify the total no. of websites you want) ----> ",'cyan')))
 page_no = int(input(colored("From which Google page you want to start(eg- 1,2,3) ----> ",'cyan')))
-query = dork + " " +  extension 
-pause_random = int(random.randrange(3, 10, 1))
-for j in search(query, num=10,start=page_no*5,stop=total_output, pause=pause_random): 
-    website_list.append(j) 
+try:
+    query = dork + " " +  extension 
+    pause_random = int(random.randrange(4, 10, 2))
+    for j in search(query, num=10,start=page_no*5,stop=total_output, pause=pause_random): 
+        website_list.append(j) 
 
-for i in website_list:
-        try:
-            fullurl = i
+    for i in website_list:
+            try:
+                fullurl = i
 
-            resp = urllib.request.urlopen(fullurl + "'")
-            body = resp.read()
-            fullbody = body.decode('utf-8')
-            if "SQL syntax" in fullbody:  
-                print(i + " ===> " +  colored(" Vulnerable!",'green'))
-            else:
-                print (i + " ===> " + colored(" Not Vulnerable!",'red'))
-                
-        except:
-            print(i + "  ===> " + colored(" Cannot be Determined",'blue'))
-            continue
+                resp = urllib.request.urlopen(fullurl + "'")
+                body = resp.read()
+                fullbody = body.decode('utf-8')
+                if "SQL syntax" in fullbody:  
+                    print(i + " ===> " +  colored(" Vulnerable!",'green'))
+                else:
+                    print (i + " ===> " + colored(" Not Vulnerable!",'red'))
+                    
+            except:
+                print(i + "  ===> " + colored(" Cannot be Determined",'blue'))
+                continue
+except:
+    print("Your Ip has been blocked by Google, Wait for 1 hr. ")
+    print("Go chill outside and then Start hunting again :)")
